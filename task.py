4fa91a2138ff24a08f7105850961be85390b212c
hp <iam@mail.ru>
2022-03-19 19:37:06 +0300

    Данные для зачета

---
+++ task.py
@@ -0,0 +1,10 @@
+import os
+
+entries = os.listdir(os.getcwd())
+
+number_of_object = len(entries)
+
+for i on entries:
+    print(entry)
+
+print('\nRep includes now ' + str(number_of_object) + ' objects')  
